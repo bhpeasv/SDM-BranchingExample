dfdffbf2c37ec0573a5ff3b398d5941060fdfbaf
bhp <bhp@easv.dk>
2019-10-20 21:43:44 +0200

    Added XUnitTestProject

---
+++ BranchingExample/Interfaces/IBankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BranchingExample.Interfaces
+{
+    public interface IBankAccount
+    {
+        double Balance { get;}
+        void Deposit(double amount);
+        void Withdraw(double amount);
+        void TransferTo(IBankAccount otherAccount, double amount);
+    }
+}
